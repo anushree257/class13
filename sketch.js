3932f6da590839d739f5d3a28cbceb9b2a295b14
anushree257 <87478108+anushree257@users.noreply.github.com>
2021-09-03 18:27:38 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,94 +1,75 @@
-var trex, trex_running, trex_collided;
-var ground, invisibleGround, groundImage;
-var cloudImage
-
-
-
-
-var score;
-
+var garden,rabbit;
+var gardenImg,rabbitImg;
+var apple,appleImg;
+var orngle,orngleImg;
 
 function preload(){
-  trex_running = loadAnimation("trex1.png","trex2.png","trex3.png");
-  trex_collided = loadImage("trex_collided.png");
-  
-  groundImage = loadImage("ground2.png");
-
-  cloudImage = loadImage("cloud.png");
-  
- 
-  
+  gardenImg = loadImage("garden.png");
+  rabbitImg = loadImage("rabbit.png");
+  appleImg = loadImage ("apple.png");
+  orngleImg = loadImage("orangeLeaf.png");
 }
 
-function setup() {
+function setup(){
+  
+  createCanvas(400,400);
+  
+// background
+garden=createSprite(200,200);
+garden.addImage(gardenImg);
 
-  createCanvas(600,200)
-  
-  //create a trex sprite
-  trex = createSprite(50,160,20,50);
-  trex.addAnimation("running", trex_running);
-  trex.scale = 0.5;
-  
-  //create a ground sprite
-  ground = createSprite(200,180,400,20);
-  ground.addImage("ground",groundImage);
-  ground.x = ground.width /2;
-  ground.velocityX = -4;
-  
-  //creating invisible ground
-  invisibleGround = createSprite(200,190,400,10);
-  invisibleGround.visible = false;
-  
-  //generate random numbers
-  var rand =  Math.round(random(1,100))
-  console.log(rand)
+// creating rabbit
+rabbit = createSprite(180,340,30,30);
+rabbit.scale =0.09;
+rabbit.addImage(rabbitImg);
+
+var select_sprites = Math.round(random(1,2));
+
+if(frameCount % 80 == 0){
+if(select_sprites == 1){
+  createApples();
+}
+else{
+  createLeaves();
+}
+}
 
 }
+
 
 function draw() {
-  //set background color
-  background(180);
+  background(0);
   
- // console.log(trex.y)
-  
-  
-  
-  // jump when the space key is pressed
-  if(keyDown("space")&& trex.y >= 100) {
-    trex.velocityY = -10;
-  }
-  
-  trex.velocityY = trex.velocityY + 0.8
-  
-  if (ground.x < 0){
-    ground.x = ground.width/2;
-  }
-  
-  //stop trex from falling down
-  trex.collide(invisibleGround);
-  
-  //Spawn Clouds
-  spawnClouds()
-  
+  edges= createEdgeSprites();
+  rabbit.collide(edges);
+
+  //to move the rabbit along with the mouse
+  rabbit.x = World.mouseX;
+
+  createApples();
+  createLeaves();
+
   drawSprites();
 }
 
-//function to spawn the clouds
-function spawnClouds(){
- // write your code here
- if(frameCount% 60 == 0) {
-  cloud=createSprite(600,100,40,10);
-  cloud.addImage(cloudImage);
-  cloud.velocityX=-3;
-  cloud.y = Math.round(random(10,100));
-  console.log(trex.depth);
-  console.log(cloud.depth);
-  cloud.depth = trex.depth;
-  trex.depth = trex.depth+1;
- }
- 
-
+//creating the function createApples
+function createApples(){
+if(frameCount % 80 == 0){
+apple = createSprite(random(50,350),40,10,10);
+apple.addImage(appleImg);
+apple.scale = 0.05;
+apple.velocityY = 4;
+apple.lifeTime = 100;
+}
 }
 
-
-
+//creating the function createLeaves
+function createLeaves(){
+  if(frameCount % 80 == 0){
+  orngle = createSprite(random(50,300),40,10,10);
+  orngle.addImage(orngleImg);
+  orngle.scale = 0.05;
+  orngle.velocityY = 4;
+  orngle.lifeTime = 100;
+  }
+}
